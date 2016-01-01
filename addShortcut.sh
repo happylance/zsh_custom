c9fa033defb85425b526fbca11536969c01593ff
Liang Liu <nlprliu@gmail.com>
2016-01-01 16:30:43 -0500

    Fixed an issue when calling this script from other directory.

--- addShortcut.sh
+++ addShortcut.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+. "$HOME/.zshrc"
+[ -z "$ZSH_CUSTOM" ] && { echo "\$ZSH_CUSTOM is not defined."; exit 1; }
+
+DIR="$ZSH_CUSTOM"
 _echo_usage () {
     cat << EOF
 usage: $0 [alias] ["command"]
@@ -13,6 +16,9 @@ EOF
 [ -z "$2" ] && _echo_usage
 
 . "$HOME/.zshrc"
+
+set -e
+
 which $1 1>/dev/null && { echo "$1 is already defined as follows."; which $1; exit 1; }
 
 shortcut_file=shortcuts.zsh
