e455c43662006dd62d635cd755e019532963b6cb
Liang Liu <nlprliu@gmail.com>
2016-01-02 21:42:23 -0500

    Added gd1, gdh1 and gaud.

--- shortcuts.zsh
+++ shortcuts.zsh
@@ -4,6 +4,7 @@ alias gau="git add -u"
 alias gcfd="git clean -fd"
 alias gcnd="git clean -nd"
 alias gd="git diff"
+alias gdh1="git diff HEAD^!"
 alias gdc="git diff --cached"
 alias gdco="git difftool -y --cached"
 alias gdd="git difftool"
@@ -16,5 +17,7 @@ alias scgp='DIR="$(pwd)"; cd "$ZSH_CUSTOM"; git push; cd "$DIR";'
 alias sdr="screen -dR"
 alias wh='which'
 ez () { vim "$ZSH_CUSTOM/shortcuts.zsh" }
+gaud() { git add -u "$@"; git diff --cached; }
+gd1() { git diff "$1"^! }
 rz () { exec zsh }
 sz () { . ~/.zshrc }
